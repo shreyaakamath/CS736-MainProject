3fb892aceab99c19e0ae85c996eb125419a2c862
shreyakamath2311 <shreyakamath@cs.wisc.edu>
2016-04-20 23:34:59 -0500

    removed bug

--- installWebServices.sh
+++ installWebServices.sh
@@ -3,7 +3,7 @@ sudo apt-get update
 sudo apt-get --assume-yes install apache2-utils
 sudo apt-get --assume-yes install nginx
 sudo service nginx stop
-sudo apt-get --assume-yes install apache2-bin
+sudo apt-get --assume-yes install apache2
 sudo service apache2 stop
 sudo apt-get --assume-yes install lighttpd
 sudo /etc/init.d/lighttpd/ stop
