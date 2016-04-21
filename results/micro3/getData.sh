3539e60c4cfa24be1377f8454ad73233c4f844d9
Shruthi Racha <shruthir@adelie-01.cs.wisc.edu>
2016-04-21 10:55:38 -0500

    2nd Set of Data

---
+++ results/micro3/getData.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+name=$1
+server=$2
+instance=$3
+echo $name
+echo $server
+echo $instance
+#server=54.186.41.160
+#instance="ec2-54-186-41-160.us-west-2.compute.amazonaws.com"
+perm="ubuntu-common-2.pem"
+ssh -i $perm $instance  -t "sudo service apache2 start"
+start=10
+for i in {1..100} 
+do
+	ab -n 10000 -c $start http://$server/ >> ${name}_apache2_c_${start}
+	start=$(($start+10))
+done
+ssh -i $perm $instance  -t "sudo service apache2 stop"
+
+ssh -i $perm $instance  -t "sudo service nginx start"
+start=10
+for i in {1..100} 
+do
+	ab -n 1000 -c $i http://$server/ >> ${name}_nginx_c_${start}
+	start=$(($start+10))
+done
+ssh -i $perm $instance  -t "sudo service nginx stop"
+
+
+ssh -i $perm $instance  -t "sudo /etc/init.d/lighttpd start"
+start=10
+for i in {1..100} 
+do
+	ab -n 1000 -c $i http://$server/ >> ${name}_lighttpd_c_${start}
+	start=$(($start+10))
+done
+ssh -i $perm $instance  -t "sudo /etc/init.d/lighttpd stop"
